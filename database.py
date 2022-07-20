27d0a57273fca32d65c7f46cab1a4b2dd03355e6
Sai Ram <86945018+sairam6381@users.noreply.github.com>
2022-07-20 21:48:26 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,55 @@
 import sqlite3
-#con=sqlite3.connect("theatre.db")
-#c=con.cursor()
+con=sqlite3.connect("theatre.db")
+c=con.cursor()
+
+# **FOR REFERENCE**
+#con.commit()
 #c.execute('''DELETE FROM login WHERE uname="sree";''')
+#c.execute(""" create table login(uname text,mailid text,password varchar)""")
+#c.execute("""create table JOHN_WICK(seat varchar)""")
+
+#c.execute("""SELECT name FROM sqlite_master WHERE type='table';""")
+#myresult = c.fetchall()
+#for x in myresult:
+#    print(x)
 #con.commit()
 
-#c.execute(""" create table login(uname text,mailid text,password varchar)""")
-#print("created")
+#Listed Movies : [AVATAR,INCEPTION,JOHN_WICK,MALEFICENT]
 
+def show(movie):
+    con=sqlite3.connect("theatre.db")
+    c=con.cursor()
+    c.execute("select * from %s"%(movie))
+    r=c.fetchall()
+    for i in r:
+        print(i)
+    con.commit()
+    con.close()
+
+def delseat(movie):
+    con=sqlite3.connect("theatre.db")
+    c=con.cursor()
+    c.execute("""delete from %s"""%(movie))
+    con.commit()
+    con.close()
+
+def filledseat(movie):
+    se=[]
+    con=sqlite3.connect("theatre.db")
+    c=con.cursor()
+    c.execute("select seat from %s"%(movie))
+    r=c.fetchall()
+    for i in r:
+        se.append(str(i[0]))
+    return se
+
+def addseat(movie,seats):
+    con=sqlite3.connect("theatre.db")
+    c=con.cursor()
+    for i in seats:
+        c.execute("""insert into %s values ('%s')"""%(movie,i))
+    con.commit()
+    con.close()
 
 def add(uname,mailid,password):
     con=sqlite3.connect("theatre.db")
@@ -14,39 +57,17 @@ def add(uname,mailid,password):
     c.execute("insert into login values(?,?,?)",(uname,mailid,password))
     con.commit()
     con.close()
-
     
-def show():
-    con=sqlite3.connect("theatre.db")
-    c=con.cursor()
-    c.execute("select * from login")
-    r=c.fetchall()
-    return r
-
-
-def show1(name):
-    con=sqlite3.connect("theatre.db")
-    c=con.cursor()
-    r=c.execute("select uname from login where uname=(?)",(name,))
-	    #r=c.fetchone
-    for i in r:
-        print(i)
-        if name in i:
-            print("yes")
-        else:
-            print("no")
-    #return r
-
-
 def check(name):
     con=sqlite3.connect("theatre.db")
     c=con.cursor()
     r=c.execute("select uname from login where uname=(?)",(name,))
     for i in r:
-        #print(i)
         if name in i:
             return 1
     return 0
+    con.commit()
+    con.close()
 
 def check_p(name,passw):
     con=sqlite3.connect("theatre.db")
@@ -57,7 +78,6 @@ def check_p(name,passw):
         if passw in i:
             return 1
     return 0
+    con.commit()
+    con.close()
     
-#print(show())
-#show1("saba")
-#print(check("sai"))
