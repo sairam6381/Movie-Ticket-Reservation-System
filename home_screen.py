e5e9498373a69c4eb3f7958a666b67e4468d0683
Sai Ram <86945018+sairam6381@users.noreply.github.com>
2022-07-20 21:49:06 +0530

    Update home_screen.py

--- home_screen.py
+++ home_screen.py
@@ -1,28 +1,12 @@
 from tkinter import *
-#import smtplib
 from tkinter import messagebox
 from PIL import ImageTk, Image
 import clickonseat
-#import clickonseat
-#if __name__=="__main__":
- #   global root_h
-  #  root_h=Tk()
-   # root_h.geometry("800x500")
-    #root_h.title("Movie selection")
-  #  root_h.iconbitmap(default='image/dragon.ico')
-   # root_h.resizable(False, False)
-    #root_h.config(background="black")
-#else:
-   # pass
 
-
-
-
-def home():
+def home(uname):
     def movie(n):
-        #print(n)
         root_h.destroy()
-        clickonseat.seat(n)
+        clickonseat.seat(n,uname)
 
     global root_h
     root_h=Tk()
@@ -31,18 +15,13 @@ def home():
     root_h.iconbitmap(default='image/dragon.ico')
     root_h.resizable(False, False)
     root_h.config(background="black")
-    
-
-    
     Label(root_h,text="SELECT THE MOVIE",font=("times new roman",18),bg="black",fg="white").grid(row=0,column=0,pady=30,padx=300,columnspan=8)
     Label(root_h,text=" ",font=("times new roman",15),bg="black").grid(row=1,column=0,padx=18)
 
     img1 = ImageTk.PhotoImage(Image.open("image/movies/avathar.jpg"))  
-    l=Button(root_h,image=img1,command=lambda:movie("AVATAR"))
-    l.grid(row=1,column=1)
+    Button(root_h,image=img1,command=lambda:movie("AVATAR")).grid(row=1,column=1)
     Label(root_h,text="Avatar",font=("times new roman",15),bg="black",fg="white").grid(row=2,column=1)
     Label(root_h,text="CBFC: U/A\nSci-fi/Action\n2h 42m",font=("times new roman",11),bg="black",fg='grey').grid(row=3,column=1)
-
 
     img2 = ImageTk.PhotoImage(Image.open("image/movies/inception.jpg"))  
     Button(root_h,image=img2,command=lambda:movie("INCEPTION")).grid(row=1,column=2)
@@ -50,7 +29,7 @@ def home():
     Label(root_h,text="CBFC: U/A\nAction/Sci-fi\n2h 28m",font=("times new roman",11),bg="black",fg='grey').grid(row=3,column=2)
 
     img3 = ImageTk.PhotoImage(Image.open("image/movies/john wick.jpg"))  
-    Button(root_h,image=img3,command=lambda:movie("JOHN WICK")).grid(row=1,column=3)
+    Button(root_h,image=img3,command=lambda:movie("JOHN_WICK")).grid(row=1,column=3)
     Label(root_h,text="John Wick",font=("times new roman",15),bg="black",fg="white").grid(row=2,column=3)
     Label(root_h,text="CBFC: U/A\nAction/Thriller\n1h 41m",font=("times new roman",11),bg="black",fg='grey').grid(row=3,column=3)
 
@@ -59,11 +38,6 @@ def home():
     Label(root_h,text="Maleficent",font=("times new roman",15),bg="black",fg="white").grid(row=2,column=4)
     Label(root_h,text="CBFC: U/A\nFantasy/Adventure\n1h 38m",font=("times new roman",11),bg="black",fg='grey').grid(row=3,column=4)
 
-    #l.place(x=0,y=0,relwidth=1,relheight=1)
+    if uname=="ADMIN":
+        messagebox.showinfo("INFO", "This is ADMIN account\n Here you can reset seat availability ")
     root_h.mainloop()
-
-    
-
-
-  
-#home()
