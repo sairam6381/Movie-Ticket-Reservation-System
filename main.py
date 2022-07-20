d3046dcbc30f6c4bfe39da940ddf17883af83e00
Sai Ram <86945018+sairam6381@users.noreply.github.com>
2022-07-20 21:49:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,7 @@ import signup
 import signin
 
 def main_w():
-    root= Tk()
-    root.geometry("600x364")
-    root.iconbitmap(default='image/dragon.ico')
-    root.title("CINEMA BUFFER")
 
-    img = ImageTk.PhotoImage(Image.open("image/fg.jpg"))  
-    l=Label(root,image=img)
-    l.place(x=0,y=0,relwidth=1,relheight=1)
-
-    root.wm_attributes('-transparentcolor', '#ab23ff')
-    root.resizable(False, False)
-
-    #functions
     def signin1():
         root.destroy()
         signin.sin()
@@ -25,8 +13,16 @@ def main_w():
         root.destroy()
         signup.sup()
 
-    #labels
-    #name=Label(root,text="WELCOME TO MOVIECATES",bg='black',fg='white',font=("Times New Roman",18)).grid(row=2,column=1,columnspan=3,pady=70,padx=70)
+    root= Tk()
+    root.geometry("600x364")
+    root.iconbitmap(default='image/dragon.ico')
+    root.title("CINEMA BUFFER")
+    root.wm_attributes('-transparentcolor', '#ab23ff')
+    root.resizable(False, False)
+
+    img = ImageTk.PhotoImage(Image.open("image/fg.jpg"))  
+    l=Label(root,image=img)
+    l.place(x=0,y=0,relwidth=1,relheight=1)
 
     #buttons
     Label(root,text="",bg="black").grid(row=0,column=0,padx=1000,columnspan=50,pady=120)
@@ -35,5 +31,4 @@ def main_w():
     register=Button(root,text="SIGN UP",bg='black',fg='white',padx=16,pady=10,command=signup1).grid(row=2,column=2)
 
     root.mainloop()
-
 main_w()
