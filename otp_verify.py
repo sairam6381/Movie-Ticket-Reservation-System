919a75d381ead5cc5543aa553a0171e96b1a2ccf
Sai Ram <86945018+sairam6381@users.noreply.github.com>
2022-07-20 21:59:48 +0530

    Update otp_verify.py

--- otp_verify.py
+++ otp_verify.py
@@ -1,31 +1,24 @@
 import smtplib
 from twilio.rest import Client
 
+#INSTRUCTION : HERE YOU WANT TO FILL THE DETAILS CONTAINING ASTERISK(*) 
+
 def mail_verify(otp,ma):
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
-    s.login("cinemabufferv.0@gmail.com", "eccohbbjewsoiewn")
+    s.login(*YOUR_MAILID*, *YOUR_PASSWORD*)
     message = "Subject:OTP Verification\n\nDear Customer,\n"+otp+" is your one time password(OTP)"
-    #print(otp)
-    s.sendmail("python6381@gmail.com", ma,message)
+    print("OTP is : ",otp)
+    s.sendmail(*YOUR_MAILID*, ma,message)
     s.quit()
-    print("mail done")
+    print("OTP Sent to Mail")
 
 def number_verify(otp,num):
-    account_sid = "AC3fc04fc188d73b2aef88bee8a6362513"
-    auth_token  = "ec25c152d9fa12358186bc1f1ed70591"
+    account_sid = *YOUR Account SID*
+    auth_token  = *YOUR Auth Token*
     client = Client(account_sid, auth_token)
     message = client.messages.create(
         to="+91"+num, 
-        from_="+15716014300",
+        from_="+19379322457",
         body="YOUR OTP IS : "+otp)
-    print(message.sid)
-    print("number done")
-
-s=3765
-#number_verify(s)
-#ot="your otp is :"
-#print(ot)
-#mail_verify(str(s),"sairam9488144883@gmail.com")
-
-
+    print("OTP Sent to Number")
